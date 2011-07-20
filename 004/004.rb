fce6884b78b9084dc44b07a30ed7c1591a659ea7
Ali Dogan <alidogan.just.for.commits@gmail.com>
2011-07-21 00:57:20 +0200

    new solution 004
    previous was invalid, wrongly coded.
    Quickly made so no comments and not
     the most optimized piece of code.

--- 004/004.rb
+++ 004/004.rb
@@ -1,20 +1,14 @@
-switch = 1
-nr1 = 999
-nr2 = 999
-
-while nr1 > 99 
-  result = nr1 * nr2
-  puts result
-  if result.to_s == result.to_s.reverse
-    break
-  end
-  if switch == 1
-    nr1 = nr1 - 1
-    switch = 2
-  else
-    nr2 = nr2 - 1
-    switch = 1
+x =0
+largest = 0
+for nr1 in 100..999
+  for nr2 in 100..999
+    result = (nr1 * nr2).to_s
+    if result == result.reverse
+      if result.to_i > largest
+        largest = result.to_i
+      end
+      break
+    end
   end
 end
-  
-puts result
+print largest
